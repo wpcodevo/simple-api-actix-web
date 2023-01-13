7b435f6d335045b850de817b165fc9eb3c2120ee
CODEVO <contact@codevoweb.com>
2023-01-13 17:27:40 +0000

    added actix-cors

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ start-server:
 
 install:
 	cargo add actix-web
+	cargo add actix-cors
 	cargo add serde --features derive
 	cargo add chrono --features serde
 	cargo add env_logger
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ mod handler;
 mod model;
 mod response;
 
+use actix_cors::Cors;
 use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpServer};
+use actix_web::{http::header, web, App, HttpServer};
 use model::AppState;
 
 #[actix_web::main]
@@ -19,9 +20,20 @@ async fn main() -> std::io::Result<()> {
     println!("🚀 Server started successfully");
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_origin("http://localhost:3000/")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
         App::new()
             .app_data(app_data.clone())
             .configure(handler::config)
+            .wrap(cors)
             .wrap(Logger::default())
     })
     .bind(("127.0.0.1", 8000))?
